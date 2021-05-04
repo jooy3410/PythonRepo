b3a84af08264b3bfac5070dba8852c1e0b8c745b
jooy3410 <jooy3410@naver.com>
2021-05-04 22:42:37 +0900

    2480

--- Baek1934.py
+++ Baek1934.py
@@ -1,13 +1,1 @@
-n = int(input())
-for _ in range(n):
-    a, b = map(int, input().split())
-    #기존의 값을 복사해둠
-    x = a
-    y = b
-    while b != 0:
-        r = a % b
-        a = b
-        b = r
-    gcd = a
-    lcd = x * y / a
-    print(int(lcd))
+case = int(input())
